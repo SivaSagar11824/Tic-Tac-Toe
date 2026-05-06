eb2dcac9865aa93d1e2cc5b3eca9b0d23f857a70
Siva Sagar <karrisivasagar1807@gmail.com>
2026-05-06 14:11:41 +0530

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,28 @@
+public class TicTacToe {
+
+    static char[][] board = {
+            {'-', '-', '-'},
+            {'-', '-', '-'},
+            {'-', '-', '-'}
+    };
+
+    public static void main(String[] args) {
+        System.out.println(isValidMove(1, 1));
+    }
+
+    /**
+     * Checks if the given row and column are within bounds
+     * and if the target cell is empty.
+     * Input: Row, Column
+     * Output: true if valid, false otherwise.
+     */
+    static boolean isValidMove(int row, int col) {
+        if (row < 0 || row > 2 || col < 0 || col > 2) {
+            return false;
+        }
+        if (board[row][col] != '-') {
+            return false;
+        }
+        return true;
+    }
+}
